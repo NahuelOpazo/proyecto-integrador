b598efbe36a439ec09c9a2127e4aaf1fe7e6fb33
Nahuel Opazo <nahuelopazo@gmail.com>
2025-10-23 00:36:22 -0300

    Init repo - estructura base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(){
+  return (
+    <div>
+      <header><h1>Librería - [Nombre]</h1></header>
+      <main><p>Página de inicio - app en desarrollo</p></main>
+    </div>
+  )
+}
+export default App
+
